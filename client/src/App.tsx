c4e8fd7a66448097a36ace16738213b7e9d4e75f
john_cook36 <bormancmexv@gmai.com>
2023-03-21 13:11:10 +0300

    Feat(project) add client in project

---
+++ client/src/App.tsx
@@ -0,0 +1,88 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { ErrorComponent, Layout, notificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import routerBindings, {
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import {
+  UserList,
+  UserCreate,
+  UserEdit,
+  UserShow,
+} from "pages/products";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { Header } from "./components";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <Refine
+            dataProvider={dataProvider("http://localhost:3004")}
+            notificationProvider={notificationProvider}
+            routerProvider={routerBindings}
+            resources={[
+              {
+                name: "user",
+                list: "/user",
+                create: "user/create",
+                edit: "user/edit/:id",
+                show: "user/show/:id",
+                meta: {
+                  canDelete: true,
+                },
+              },
+            ]}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+            }}
+          >
+            <Routes>
+              <Route
+                element={
+                  <Layout Header={Header}>
+                    <Outlet />
+                  </Layout>
+                }
+              >
+                <Route
+                  index
+                  element={<NavigateToResource resource="user" />}
+                />
+                <Route path="/user">
+                  <Route index element={<UserList />} />
+                  <Route path="create" element={<UserCreate />} />
+                  <Route path="edit/:id" element={<UserEdit />} />
+                  <Route path="show/:id" element={<UserShow />} />
+                </Route>
+              </Route>
+              <Route
+                element={
+                  <Layout Header={Header}>
+                    <Outlet />
+                  </Layout>
+                }
+              >
+                <Route path="*" element={<ErrorComponent />} />
+              </Route>
+            </Routes>
+
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+          </Refine>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/products/show.tsx
@@ -0,0 +1,7 @@
+import { IResourceComponentsProps } from "@refinedev/core";
+import { AntdShowInferencer } from "@refinedev/inferencer/antd";
+import React from "react";
+
+export const UserShow: React.FC<IResourceComponentsProps> = () => {
+  return <AntdShowInferencer />;
+};
