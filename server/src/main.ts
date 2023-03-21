5462a453a6100706b63ba12c570bfa2cc0a5d5e5
john_cook36 <bormancmexv@gmai.com>
2023-03-21 12:42:54 +0300

    Feat(project) add client in project

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
   await app.listen(PORT, () => {
